e970f202231d6220e02d87043a093c4e54b40207
Ashley Dennis <adenn1852@gmail.com>
2022-02-20 21:57:09 -0500

    corrected functions

--- practice.js
+++ practice.js
@@ -2,27 +2,33 @@ let shoppingList = ["celery", "limes", "lemons", "grenadine", "oranges","salt"]
 
 // Array.pop() → removes the last element in the array and returns that element.
 
-let getLastItem = shoppingList.pop()
-console.log(getLastItem)
+let getLastItem = (arr => arr.pop())
+console.log(getLastItem(shoppingList))
 
 //**** Array.unshift() → adds a new element at the beginning of the array.
 
-let addItem = shoppingList.unshift("pepper")
-console.log(addItem + shoppingList)
+let addItem = (arr => arr.unshift('pepper'))
+console.log(addItem(shoppingList))
 
 // Array.shift() → removes the first element in an array and returns that element.
 
-let remFirst = shoppingList.shift()
-console.log(remFirst)
+let remFirst = (arr => arr.shift())
+console.log(remFirst(shoppingList))
 
 // **** Array.forEach() → calls a function once for each element in the array.
 
-// shoppingList.forEach(item => console.log(item))
+let changeList = [] 
+shoppingList.forEach(element => {
+    changeList.push(element.toUpperCase())
+})
+console.log(changeList)
 
 // **** Array.map() → transforms the elements in the original array by calling the given function once for each element in the array.
 
-// shoppingList.map(item => console.log(item))
-
+const capitalArr = shoppingList.map(elem =>{
+    return elem.toUpperCase()
+})
+console.log(capitalArr)
 
 // Array.filter() → creates a new array with only the elements that pass the test in a given function.
 
@@ -36,22 +42,24 @@ console.log(findItem)
 
 //*** Array.reduce() → executes a given function for each value of the array and returns a single value.
 
-//  const listOrder = shoppingList.reduce((salt, celery) => salt celery )
-//  console.log(listOrder)
+ const listOrder = shoppingList.reduce((prev, curr) => prev + ',' + curr)
+ console.log(listOrder)
 
 // Array.includes() → determines whether an array has a specific element.
 
-let findList = shoppingList.includes('grenadine')
-console.log(findList)
+let findList = arr => arr.includes('grenadine')
+console.log(findList(shoppingList))
 
 // Array.indexOf() → search the array for a specific element and returns its first index.
 
-let returnIndex = shoppingList.indexOf('oranges')
-console.log(returnIndex)
+let returnIndex = arr => arr.indexOf('oranges')
+console.log(returnIndex(shoppingList))
 
 // Array.findIndex() → returns the index of the first element in an array that passes the test in a given function.
 
-let shopIndex = shoppingList.findIndex(listIndex => listIndex === "lemons")
-console.log(shopIndex)
+let shopIndex = arr => arr.length > 6
+console.log(shoppingList.findIndex(shopIndex))
 
 // Array.every() → checks if all elements in an array pass a test given as a function
+const checkElem = arr => arr.length > 3
+console.log(shoppingList.every(checkElem))
